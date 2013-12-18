3e78b53102d6d53ae18c04bdb29b6e3b6970fde6
millzpaugh <ann.millspaugh@gmail.com>
2013-12-18 10:05:25 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LoanProject::Application.config.secret_key_base = '419d6c3c0e7d3a5d7dd627617e46518743e9beea0ab6797a9a23f4471219aa8611dd3456815a41d23753ca78f500970c4c733a79cb664c87e069190d685e851d'
