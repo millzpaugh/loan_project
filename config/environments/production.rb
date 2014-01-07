a99189c90df91feca5d45dff5b11c84ae58b6689
millzpaugh <ann.millspaugh@gmail.com>
2014-01-07 11:53:57 -0500

    graphsjs precompiled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,8 @@ LoanProject::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.assets.precompile += %w[ graphs/*.js ]
+
 
   # Generate digests for assets URLs.
   config.assets.digest = true
